9ef53405ec2a27037fcad2811e414f425c2beb91
Rubén García Pérez <Ruben@MacBook-Air-de-Ruben.local>
2013-07-23 23:55:36 +0200

    Initial Commit Signed-off-by: Rubén García Pérez <Ruben@MacBook-Air-de-Ruben.local>

---
+++ Turbo Boost Disabler/StartupHelper.h
@@ -0,0 +1,36 @@
+//
+//  StartupHelper.h
+//  Turbo Boost Switcher
+//
+//  Created by Rubén García Pérez on 21/07/13.
+//  Copyright (c) 2013 Rubén García Pérez.
+//  rugarciap.com
+//
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface StartupHelper : NSObject {
+    
+}
+
++ (BOOL) isOpenAtLogin;
+
++ (void) setOpenAtLogin:(BOOL) isOpenAtLogin;
+
+
+@end
